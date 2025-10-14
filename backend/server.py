fe53194515ceee093a4a20ae9739260afcaa9efd
Mohammed Bahageel <Mohmmed.Bahageel@DSAH.SA>
2025-10-14 10:36:17 +0300

    deploying the backend server

--- backend/server.py
+++ backend/server.py
@@ -35,7 +35,7 @@ app = Flask(__name__)
 CORS(app, origins=[
     "http://localhost:3000",
     "https://medicaltranscription-version2-tests.onrender.com",
-    os.getenv("CORS_ORIGIN", "")
+   "https://medicaltranscription-version2.onrender.com"
 ])
 
 # -------------------- VECTOR STORE --------------------
